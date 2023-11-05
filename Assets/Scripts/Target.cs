60c98eeacfabbfff46066a4b08811ec3f46c2250
ploidy <147208413+ploidy@users.noreply.github.com>
2023-11-05 22:10:10 +0000

    5.2 complete
    lesson 5.2 complete

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Target : MonoBehaviour
@@ -8,17 +9,23 @@ public class Target : MonoBehaviour
 
 
     private Rigidbody targetRb;
+    private GameManager gameManager;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -3;
 
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+
 
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>(); //finds gamemanager script
 
         targetRb.AddForce(RandomForce(), ForceMode.Impulse); //applies upward force on start
         targetRb.AddTorque(RandomTorque(), RandomTorque(),RandomTorque(), ForceMode.Impulse); //applies rotating force
@@ -31,6 +38,19 @@ public class Target : MonoBehaviour
     {
         
     }
+   
+    private void OnMouseDown() 
+    { //when mouse is 'down' inside object collider
+        Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue); //uses updatescore method from gamemanager script
+
+    }
+    private void OnTriggerEnter(Collider other) {
+        Destroy(gameObject);
+    }
+    
+    
     Vector3 RandomForce(){
         return Vector3.up * Random.Range(minSpeed, maxSpeed);
     }
@@ -39,12 +59,5 @@ public class Target : MonoBehaviour
     }
     Vector3 RandomSpawnPos(){
         return new Vector3(Random.Range(-xRange,xRange),ySpawnPos);
-    }
-    private void OnMouseDown() { //when mouse is 'down' inside object collider
-        Destroy(gameObject);
-    }
-
-    private void OnTriggerEnter(Collider other) {
-        Destroy(gameObject);
     }
 }
